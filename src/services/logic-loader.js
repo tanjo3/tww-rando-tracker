510e0b82656defee0453e2299f79f9c39d0d2ca9
Jonathan Tan <jontan_@hotmail.com>
2022-09-05 12:42:56 -0400

    Point logic files to archived dv_im

--- src/services/logic-loader.js
+++ src/services/logic-loader.js
@@ -27,6 +27,6 @@ export default class LogicLoader {
   }
 
   static _logicFileUrl(fileName) {
-    return `https://raw.githubusercontent.com/DualVission/twwrando-dv_im/${Settings.getVersion()}/logic/${fileName}`;
+    return `https://raw.githubusercontent.com/DualVission/archive-twwrando-dv_im/${Settings.getVersion()}/logic/${fileName}`;
   }
 }
